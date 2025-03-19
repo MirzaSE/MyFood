c711c06e19f94f547fe45651694f3fb6e6627c46
Mirza <mirzase2@gmail.com>
2025-03-19 16:08:42 +0100

    Add SearchByName route

--- MyFood.Infrastructure/Repositories/IFoodRepository.cs
+++ MyFood.Infrastructure/Repositories/IFoodRepository.cs
@@ -11,6 +11,8 @@ namespace MyFood.Infrastructure.Repositories
         FoodEntity Update(int id, FoodEntity item);
         IQueryable<FoodEntity> GetAll(QueryParameters queryParameters);
         ICollection<FoodEntity> GetRandomMeal();
+
+        IEnumerable<FoodEntity> SearchFoodsByName(string name);
         int Count();
         bool Save();
     }
