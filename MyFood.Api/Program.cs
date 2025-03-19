b275769a3dbd5f2a50b25f993982d70e27a8d7f6
Ahmed210901 <ahmedpilav22@gmail.com>
2025-03-19 17:39:10 +0100

    Implement IngredientRepository and IngredientsController, plus related changes

--- MyFood.Api/Program.cs
+++ MyFood.Api/Program.cs
@@ -23,6 +23,7 @@ builder.Services.AddControllers()
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IIngredientRepository, IngredientSqlRepository>();
 
 builder.Services.AddCustomCors("AllowAllOrigins");
 
---
+++ MyFood.Domain/Entities/IngridientEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyFood.Application.Entities
+{
+    public class IngredientEntity
+    {
+        public int Id { get; set; }
+
+        [MaxLength(260)]
+        public string? Name { get; set; }
+
+        public string? Quantity { get; set; }
+
+        // Foreign Key to FoodEntity
+        public int FoodEntityId { get; set; }
+
+        [ForeignKey("FoodEntityId")]
+        public FoodEntity Food { get; set; }
+    }
+}
