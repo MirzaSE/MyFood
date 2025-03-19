80805f9874d60bc90484f6f5ca603618cd52c2ae
Ilma Makarevic <makarevic85@gmail.com>
2025-03-19 21:31:20 +0100

    Added ingredients controller

--- MyFood.Api/Controllers/v1/IngredientsController.cs
+++ MyFood.Api/Controllers/v1/IngredientsController.cs
@@ -11,13 +11,13 @@ using System.Text.Json;
 
 namespace MyFood.Api.Controllers.v1
 {
+    [Route("api/v1/[controller]")]
     [ApiController]
     [ApiVersion("1.0")]
-    [Route("api/v{version:apiVersion}/[controller]")]
     public class IngredientsController : ControllerBase
     {
         private readonly IIngredientRepository _ingredientRepository;
-        private readonly IMapper _mapper;
+         private readonly IMapper _mapper;
         private readonly ILinkService<IngredientsController> _linkService;
 
         public IngredientsController(
@@ -30,10 +30,12 @@ namespace MyFood.Api.Controllers.v1
             _linkService = linkService;
         }
 
+        // GET: api/v1/Ingredients
         [HttpGet(Name = nameof(GetAllIngredients))]
         public ActionResult GetAllIngredients(ApiVersion version, [FromQuery] QueryParameters queryParameters)
         {
-            var ingredients = _ingredientRepository.GetAll(queryParameters).ToList();
+            List<IngredientEntity> foodItems = _ingredientRepository.GetAll(queryParameters).ToList();
+
             var allItemCount = _ingredientRepository.Count();
 
             var paginationMetadata = new
@@ -47,7 +49,7 @@ namespace MyFood.Api.Controllers.v1
             Response.Headers.Add("X-Pagination", JsonSerializer.Serialize(paginationMetadata));
 
             var links = _linkService.CreateLinksForCollection(queryParameters, allItemCount, version);
-            var toReturn = ingredients.Select(x => _linkService.ExpandSingleIngredientItem(x, x.Id, version));
+            var toReturn = foodItems.Select(x => _linkService.ExpandSingleFoodItem(x, x.Id, version));
 
             return Ok(new
             {
@@ -55,9 +57,9 @@ namespace MyFood.Api.Controllers.v1
                 links = links
             });
         }
-
-        [HttpGet("{id:int}", Name = nameof(GetSingleIngredient))]
-        public ActionResult GetSingleIngredient(ApiVersion version, int id)
+        // GET: api/v1/Ingredients/5
+        [HttpGet("{id:int}")]
+        public ActionResult<IngredientEntity> GetIngredient(int id)
         {
             var ingredient = _ingredientRepository.GetSingle(id);
 
@@ -66,75 +68,57 @@ namespace MyFood.Api.Controllers.v1
                 return NotFound();
             }
 
-            var ingredientDto = _mapper.Map<IngredientDto>(ingredient);
-
-            return Ok(_linkService.ExpandSingleIngredientItem(ingredientDto, ingredientDto.Id, version));
+            return Ok(ingredient);
         }
 
-        [HttpPost(Name = nameof(AddIngredient))]
-        public ActionResult<IngredientDto> AddIngredient(ApiVersion version, [FromBody] IngredientCreateDto ingredientCreateDto)
+        // POST: api/v1/Ingredients
+        [HttpPost]
+        public ActionResult AddIngredient([FromBody] IngredientEntity ingredient)
         {
-            if (ingredientCreateDto == null)
+            if (ingredient == null)
             {
                 return BadRequest();
             }
 
-            var toAdd = _mapper.Map<IngredientEntity>(ingredientCreateDto);
-            _ingredientRepository.Add(toAdd);
+            _ingredientRepository.Add(ingredient);
 
             if (!_ingredientRepository.Save())
             {
-                throw new Exception("Creating an ingredient failed on save.");
+                return StatusCode(500, "A problem happened while handling your request.");
             }
 
-            var newIngredient = _ingredientRepository.GetSingle(toAdd.Id);
-            var ingredientDto = _mapper.Map<IngredientDto>(newIngredient);
-
-            return CreatedAtRoute(nameof(GetSingleIngredient),
-                new { version = version.ToString(), id = newIngredient.Id },
-                _linkService.ExpandSingleIngredientItem(ingredientDto, ingredientDto.Id, version));
+            return CreatedAtAction(nameof(GetIngredient), new { id = ingredient.Id }, ingredient);
         }
 
-        [HttpPatch("{id:int}", Name = nameof(PartiallyUpdateIngredient))]
-        public ActionResult<IngredientDto> PartiallyUpdateIngredient(ApiVersion version, int id, [FromBody] JsonPatchDocument<IngredientUpdateDto> patchDoc)
+        // PUT: api/v1/Ingredients/5
+        [HttpPut("{id:int}")]
+        public ActionResult UpdateIngredient(int id, [FromBody] IngredientEntity ingredient)
         {
-            if (patchDoc == null)
+            if (ingredient == null || id != ingredient.Id)
             {
                 return BadRequest();
             }
 
-            var existingEntity = _ingredientRepository.GetSingle(id);
+            var existingIngredient = _ingredientRepository.GetSingle(id);
 
-            if (existingEntity == null)
+            if (existingIngredient == null)
             {
                 return NotFound();
             }
 
-            var ingredientUpdateDto = _mapper.Map<IngredientUpdateDto>(existingEntity);
-            patchDoc.ApplyTo(ingredientUpdateDto);
-
-            TryValidateModel(ingredientUpdateDto);
-
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
-
-            _mapper.Map(ingredientUpdateDto, existingEntity);
-            var updated = _ingredientRepository.Update(id, existingEntity);
+            _ingredientRepository.Update(id, ingredient);
 
             if (!_ingredientRepository.Save())
             {
-                throw new Exception("Updating an ingredient failed on save.");
+                return StatusCode(500, "A problem happened while handling your request.");
             }
 
-            var ingredientDto = _mapper.Map<IngredientDto>(updated);
-
-            return Ok(_linkService.ExpandSingleIngredientItem(ingredientDto, ingredientDto.Id, version));
+            return NoContent();
         }
 
-        [HttpDelete("{id:int}", Name = nameof(RemoveIngredient))]
-        public ActionResult RemoveIngredient(int id)
+        // DELETE: api/v1/Ingredients/5
+        [HttpDelete("{id:int}")]
+        public ActionResult DeleteIngredient(int id)
         {
             var ingredient = _ingredientRepository.GetSingle(id);
 
@@ -147,56 +131,10 @@ namespace MyFood.Api.Controllers.v1
 
             if (!_ingredientRepository.Save())
             {
-                throw new Exception("Deleting an ingredient failed on save.");
+                return StatusCode(500, "A problem happened while handling your request.");
             }
 
             return NoContent();
-        }
-
-        [HttpPut("{id:int}", Name = nameof(UpdateIngredient))]
-        public ActionResult<IngredientDto> UpdateIngredient(ApiVersion version, int id, [FromBody] IngredientUpdateDto ingredientUpdateDto)
-        {
-            if (ingredientUpdateDto == null)
-            {
-                return BadRequest();
-            }
-
-            var existingIngredient = _ingredientRepository.GetSingle(id);
-
-            if (existingIngredient == null)
-            {
-                return NotFound();
-            }
-
-            _mapper.Map(ingredientUpdateDto, existingIngredient);
-            _ingredientRepository.Update(id, existingIngredient);
-
-            if (!_ingredientRepository.Save())
-            {
-                throw new Exception("Updating an ingredient failed on save.");
-            }
-
-            var ingredientDto = _mapper.Map<IngredientDto>(existingIngredient);
-
-            return Ok(_linkService.ExpandSingleIngredientItem(ingredientDto, ingredientDto.Id, version));
-        }
-
-        [HttpGet("GetRandomMeal", Name = nameof(GetRandomMeal))]
-        public ActionResult GetRandomMeal()
-        {
-            var ingredients = _ingredientRepository.GetRandomMeal();
-            var dtos = ingredients.Select(x => _mapper.Map<IngredientDto>(x));
-
-            var links = new List<LinkDto>
-            {
-                new LinkDto(Url.Link(nameof(GetRandomMeal), null), "self", "GET")
-            };
-
-            return Ok(new
-            {
-                value = dtos,
-                links = links
-            });
         }
     }
 }
