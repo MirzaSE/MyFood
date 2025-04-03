cde1f3c676d268d5734edcac034a5153681c69e5
DeFactin <aminadefacto47@gmail.com>
2025-04-04 01:22:40 +0200

    email_format

--- MyFood.Application/Dtos/RegisterDto.cs
+++ MyFood.Application/Dtos/RegisterDto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,16 @@ namespace MyFood.Application.DTOs
 {
     public class RegisterDto
     {
-        public string Username { get; set; }
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
         public string Password { get; set; }
+
     }
 }
 
