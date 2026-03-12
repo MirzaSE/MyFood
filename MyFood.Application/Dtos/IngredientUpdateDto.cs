de0e8497f4d370c0569730a469e631cd63fa8506
eahmethodzic97 <edis.ahmethodzic@hotmail.com>
2026-03-12 12:58:14 +0100

    DTOs

---
+++ MyFood.Application/Dtos/IngredientUpdateDto.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MyFood.Application.Dtos
+{
+    public class IngredientUpdateDto
+    {
+        [Required]
+        [MaxLength(260)]
+        public string? Name { get; set; }
+        public string? Quantity { get; set; }
+    }
+}
