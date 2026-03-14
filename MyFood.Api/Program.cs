92ce00ba39ec3615f74fe6fcef8921ed82027c9a
MrR1ad <riad.elezovic@outlook.com>
2026-03-14 18:38:29 +0100

    Configured application services and middleware
    Updated Program.cs to register controllers, database context and application services required by the API.

--- MyFood.Api/Program.cs
+++ MyFood.Api/Program.cs
@@ -35,7 +35,7 @@ builder.Services.AddSingleton<ISeedDataService, SeedDataService>();
 builder.Services.AddScoped<IFoodRepository, FoodSqlRepository>();
 builder.Services.AddScoped(typeof(ILinkService<>), typeof(LinkService<>));
 builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
-
+builder.Services.AddScoped<IIngredientRepository, IngredientSqlRepository>();
 builder.Services.AddSingleton<IActionContextAccessor, ActionContextAccessor>();
 builder.Services.AddSingleton<IUrlHelperFactory, UrlHelperFactory>();
 
