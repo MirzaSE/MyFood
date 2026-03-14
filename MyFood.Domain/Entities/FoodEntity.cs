e4c5d03c376e5f207354fdab1df957cb0bf08305
MrR1ad <riad.elezovic@outlook.com>
2026-03-14 18:33:06 +0100

    Added FoodEntity domain model
    Introduced the FoodEntity class used to represent food items in the system and establish relationships with ingredients.

--- MyFood.Domain/Entities/FoodEntity.cs
+++ MyFood.Domain/Entities/FoodEntity.cs
@@ -1,15 +1,23 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyFood.Application.Entities
 {
     public class FoodEntity
     {
         public int Id { get; set; }
+
         [MaxLength(250)]
         public string? Name { get; set; }
+
         [MaxLength(50)]
         public string? Type { get; set; }
+
         public int Calories { get; set; }
+
         public DateTime Created { get; set; }
+
+        public List<IngredientEntity> Ingredients { get; set; } = new List<IngredientEntity>();
     }
 }
