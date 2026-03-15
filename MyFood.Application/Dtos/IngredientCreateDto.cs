f41e64d36d9fca9cc8424cafe4e7d51fe9ebdc19
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-03-15 14:25:01 +0100

    Add Ingredient DTOs

---
+++ MyFood.Application/Dtos/IngredientCreateDto.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MyFood.Application.Dtos
+{
+    public class IngredientCreateDto
+    {
+        [Required]
+        public string? Name {get; set;}
+        public int Quantity {get; set;}
+    }
+}
