3aeee77cf3b2aa9609e8763e84332a2f9c3e6958
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-03-15 15:58:16 +0100

    Add ingredient migrations

---
+++ MyFood.Infrastructure/Migrations/20260315141642_AddIngredients.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyFood.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIngredients : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "IngredientEntities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", maxLength: 260, nullable: false),
+                    FoodEntityId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_IngredientEntities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_IngredientEntities_FoodItems_FoodEntityId",
+                        column: x => x.FoodEntityId,
+                        principalTable: "FoodItems",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_IngredientEntities_FoodEntityId",
+                table: "IngredientEntities",
+                column: "FoodEntityId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "IngredientEntities");
+        }
+    }
+}
