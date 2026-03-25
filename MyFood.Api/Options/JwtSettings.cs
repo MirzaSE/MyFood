42c4e39ac8c6c560c3ebd2108e26d46a418875f7
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-03-25 19:05:42 +0100

    Add a simple poco

---
+++ MyFood.Api/Options/JwtSettings.cs
@@ -0,0 +1,8 @@
+namespace MyFood.Api.Options;
+public class JwtSettings
+{
+    public string Key { get; set; } = string.Empty;
+    public string Issuer { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+    public int DurationInMinutes { get; set; }
+}
