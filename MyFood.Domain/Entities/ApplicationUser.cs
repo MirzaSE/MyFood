67e9d34ba94d85aeaa54697844346b83b4b18805
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-03-25 18:09:02 +0100

    Add ApplicationUser entity

--- MyFood.Domain/Entities/ApplicationUser.cs
+++ MyFood.Domain/Entities/ApplicationUser.cs
@@ -1,8 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace MyFood.Domain.Entities
 {
     public class ApplicationUser : IdentityUser
     {
+        [MaxLength(500)]
+        public string? FullName {get; set;}
     }
 }
