34dd0bcb8d0499fef064316249736a7206fb3e00
MrR1ad <riad.elezovic@outlook.com>
2026-03-26 01:15:00 +0100

    Assignment 2
    Added Domain instead of Application

--- MyFood.Domain/Entities/IngredientEntity.cs
+++ MyFood.Domain/Entities/IngredientEntity.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace MyFood.Application.Entities
+namespace MyFood.Domain.Entities
 {
     public class IngredientEntity
     {
