076f1de7694ad966f42a78c6b79c81958d20cee8
MrR1ad <riad.elezovic@outlook.com>
2026-03-26 01:12:28 +0100

    Assignment 2
    Application removed and Domain added

--- MyFood.Api/Controllers/v1/IngredientsController.cs
+++ MyFood.Api/Controllers/v1/IngredientsController.cs
@@ -1,7 +1,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using MyFood.Application.Dtos;
-using MyFood.Application.Entities;
+using MyFood.Domain.Entities;
 using MyFood.Infrastructure.Repositories;
 
 namespace MyFood.Api.Controllers.v1
