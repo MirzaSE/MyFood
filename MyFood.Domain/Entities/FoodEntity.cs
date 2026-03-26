4d0337d2c2021b42cd38a8b9b7670502e828224d
MrR1ad <riad.elezovic@outlook.com>
2026-03-26 01:14:43 +0100

    Assignment 2
    Added Domain instead of Applicaiton

--- MyFood.Domain/Entities/FoodEntity.cs
+++ MyFood.Domain/Entities/FoodEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
-namespace MyFood.Application.Entities
+namespace MyFood.Domain.Entities
 {
     public class FoodEntity
     {
