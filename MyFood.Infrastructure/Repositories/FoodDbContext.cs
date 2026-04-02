33db76513a4babab6712243fc255ce5138380530
keraaa92 <121890183+keraaa92@users.noreply.github.com>
2026-04-02 13:22:10 +0200

    temp save

--- MyFood.Infrastructure/Repositories/FoodDbContext.cs
+++ MyFood.Infrastructure/Repositories/FoodDbContext.cs
@@ -1,16 +1,22 @@
 ﻿  using Microsoft.EntityFrameworkCore;
 using MyFood.Application.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyFood.Domain.Entities;
 
 namespace MyFood.Infrastructure.Repositories
 {
-    public class FoodDbContext : DbContext
+    public class FoodDbContext : IdentityDbContext<ApplicationUser>
     {
-        public FoodDbContext(DbContextOptions<FoodDbContext> options)
-            : base(options)
-        {
-        }
+       public FoodDbContext(DbContextOptions<FoodDbContext> options)
+        : base(options)
+    {
+    }
 
         public DbSet<FoodEntity> FoodItems { get; set; } = null!;
-        public DbSet<IngredientEntity> Ingredients { get; set; } 
+        public DbSet<IngredientEntity> Ingredients { get; set; } = null!;
+        
     }
+
+    
 }
