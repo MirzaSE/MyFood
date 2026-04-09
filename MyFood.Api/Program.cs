9dc2d65fdc639269b021c1887097a347f6ae6787
DrErvin <ervin.dragovic@gmail.com>
2026-04-09 23:35:05 +0200

    Squashed commit of the following:
    commit 8afdee6567e0c0cab750e8a6e44d785c1a5ab7b3
    Author: MrR1ad <riad.elezovic@outlook.com>
    Date:   Thu Apr 9 23:20:03 2026 +0200
    
        Revert "Merge assignment3 clean architecture changes into spring2026/week-7"
    
        This reverts commit 671c7aeb137dd6505cc219e10e54cda063a0641e, reversing
        changes made to 092c2ac26df386c90bcadccc158fd5146515b741.
    
    commit 671c7aeb137dd6505cc219e10e54cda063a0641e
    Merge: 092c2ac f7a05d2
    Author: MrR1ad <riad.elezovic@outlook.com>
    Date:   Thu Apr 9 23:01:11 2026 +0200
    
        Merge assignment3 clean architecture changes into spring2026/week-7
    
    commit 092c2ac26df386c90bcadccc158fd5146515b741
    Author: Mirza Selimovic <mselimovic@ius.local>
    Date:   Wed Apr 8 14:47:37 2026 +0200
    
        Arch cleanup
    
    commit f7a05d276a62c899c3a397c36bd69bf249b80c55
    Author: MrR1ad <riad.elezovic@outlook.com>
    Date:   Wed Apr 1 23:49:44 2026 +0200
    
        Refactor MyFood API to Clean Architecture for assignment 3
    
        - moved IFoodRepository from Infrastructure to Application layer
        - added IFoodService and FoodService to introduce service layer
        - added IAuthService, AuthService, and AuthResponseDto
        - updated FoodsController to depend on IFoodService instead of IFoodRepository
        - updated Program.cs dependency injection for repository and service registration
        - updated ApplicationUser to match identity/user requirements
        - kept FoodSqlRepository implementation in Infrastructure
        - aligned project structure with Clean Architecture lecture requirements
    
    commit e5f554db02202648e3233e66429bbb2155d111db
    Author: MrR1ad <riad.elezovic@outlook.com>
    Date:   Wed Apr 1 23:48:47 2026 +0200
    
        Moved from Repository to Services in Application
    
    commit 49782540e0e0e0dad351976a976be6381ee77e91
    Author: MrR1ad <riad.elezovic@outlook.com>
    Date:   Wed Apr 1 23:48:05 2026 +0200
    
        I only added a helper method to support pagination
    
        Recommended by AI
    
    commit ef31b0abd6d2c1af4cfa77391e92be106c3ae851
    Author: MrR1ad <riad.elezovic@outlook.com>
    Date:   Wed Apr 1 23:17:47 2026 +0200
    
        Deleted comments and extra s in password

--- MyFood.Api/Program.cs
+++ MyFood.Api/Program.cs
@@ -37,6 +37,7 @@ builder.Services.AddSingleton<ISeedDataService, SeedDataService>();
 builder.Services.AddScoped<IFoodRepository, FoodSqlRepository>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<IPasswordService, PasswordService>();
+builder.Services.AddScoped<IFoodService, FoodService>();
 builder.Services.AddScoped(typeof(ILinkService<>), typeof(LinkService<>));
 builder.Services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
 
---
+++ MyFood.Application/Services/FoodService.cs
@@ -0,0 +1,99 @@
+using AutoMapper;
+using MyFood.Application.Dtos;
+using MyFood.Application.Services;
+using MyFood.Domain.Entities;
+
+namespace MyFood.Application.Services
+{
+    public class FoodService : IFoodService
+    {
+        private readonly IFoodRepository _foodRepository;
+        private readonly IMapper _mapper;
+
+        public FoodService(IFoodRepository foodRepository, IMapper mapper)
+        {
+            _foodRepository = foodRepository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<FoodDto>> GetAllFoodsAsync(QueryParameters queryParameters)
+        {
+            var foodEntities = _foodRepository.GetAll(queryParameters);
+            return await Task.FromResult(_mapper.Map<IEnumerable<FoodDto>>(foodEntities));
+        }
+
+        public async Task<FoodDto?> GetFoodByIdAsync(int id)
+        {
+            var foodEntity = _foodRepository.GetSingle(id);
+            return await Task.FromResult(foodEntity != null ? _mapper.Map<FoodDto>(foodEntity) : null);
+        }
+
+        public async Task<IEnumerable<FoodDto>> SearchFoodsByNameAsync(string name)
+        {
+            var foodEntities = _foodRepository.SearchFoodsByName(name);
+            return await Task.FromResult(_mapper.Map<IEnumerable<FoodDto>>(foodEntities));
+        }
+
+        public async Task<FoodDto> CreateFoodAsync(FoodCreateDto foodCreateDto)
+        {
+            var foodEntity = _mapper.Map<FoodEntity>(foodCreateDto);
+            _foodRepository.Add(foodEntity);
+
+            if (!_foodRepository.Save())
+            {
+                throw new Exception("Creating a food item failed on save.");
+            }
+
+            var newFoodEntity = _foodRepository.GetSingle(foodEntity.Id);
+            return await Task.FromResult(_mapper.Map<FoodDto>(newFoodEntity));
+        }
+
+        public async Task<FoodDto?> UpdateFoodAsync(int id, FoodUpdateDto foodUpdateDto)
+        {
+            var existingEntity = _foodRepository.GetSingle(id);
+            if (existingEntity == null)
+            {
+                return null;
+            }
+
+            _mapper.Map(foodUpdateDto, existingEntity);
+            var updatedEntity = _foodRepository.Update(id, existingEntity);
+
+            if (!_foodRepository.Save())
+            {
+                throw new Exception("Updating a food item failed on save.");
+            }
+
+            return await Task.FromResult(_mapper.Map<FoodDto>(updatedEntity));
+        }
+
+        public async Task<bool> DeleteFoodAsync(int id)
+        {
+            var foodEntity = _foodRepository.GetSingle(id);
+            if (foodEntity == null)
+            {
+                return false;
+            }
+
+            _foodRepository.Delete(id);
+
+            if (!_foodRepository.Save())
+            {
+                throw new Exception("Deleting a food item failed on save.");
+            }
+
+            return true;
+        }
+
+        public async Task<IEnumerable<FoodDto>> GetRandomMealAsync()
+        {
+            var foodEntities = _foodRepository.GetRandomMeal();
+            return await Task.FromResult(_mapper.Map<IEnumerable<FoodDto>>(foodEntities));
+        }
+
+        public async Task<int> GetTotalFoodCountAsync()
+        {
+            return await Task.FromResult(_foodRepository.Count());
+        }
+    }
+}
