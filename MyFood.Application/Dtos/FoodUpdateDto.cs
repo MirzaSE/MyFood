d8edee88b803212969f060b48cb5ebec23f51b43
unknown <bajricalmir55@gmail.com>
2026-04-30 15:37:14 +0200

    assignment 4

--- MyFood.Application/Dtos/FoodUpdateDto.cs
+++ MyFood.Application/Dtos/FoodUpdateDto.cs
@@ -1,11 +1,18 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace MyFood.Application.Dtos
 {
     public class FoodUpdateDto
     {
+        [Required(ErrorMessage = "Name is required")]
         public string? Name { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Calories must be a positive number")]
         public int Calories { get; set; }
+
+        [Required(ErrorMessage = "Type is required")]
         public string? Type { get; set; }
+
         public DateTime Created { get; set; }
     }
 }
