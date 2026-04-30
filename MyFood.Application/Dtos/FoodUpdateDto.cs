cfc696150c04ce45abc9f8d65b7ebef3f12255dc
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-04-30 09:23:44 +0200

    feat: Assignment 4

--- MyFood.Application/Dtos/FoodUpdateDto.cs
+++ MyFood.Application/Dtos/FoodUpdateDto.cs
@@ -1,11 +1,18 @@
-﻿
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace MyFood.Application.Dtos
 {
     public class FoodUpdateDto
     {
+        [Required(ErrorMessage = "Name is required")]
+        [RegularExpression(@".*\S.*", ErrorMessage = "Name cannot be empty")]
         public string? Name { get; set; }
+
+        [Range(1, int.MaxValue, ErrorMessage = "Calories must be greater than 0")]
         public int Calories { get; set; }
+
+        [Required(ErrorMessage = "Type is required")]
+        [RegularExpression(@".*\S.*", ErrorMessage = "Type cannot be empty")]
         public string? Type { get; set; }
-        public DateTime Created { get; set; }
     }
 }
