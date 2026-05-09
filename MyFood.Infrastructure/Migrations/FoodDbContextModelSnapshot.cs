78201aec28f64963f8a4e513b47cb49f773bf338
Ahmet Tarik Duyar (atduyar) <atduyar@gmail.com>
2026-05-09 17:13:22 +0200

    Reset migrations: single InitialCreate migration from merged model

--- MyFood.Infrastructure/Migrations/FoodDbContextModelSnapshot.cs
+++ MyFood.Infrastructure/Migrations/FoodDbContextModelSnapshot.cs
@@ -17,7 +17,7 @@ namespace MyFood.Infrastructure.Migrations
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "8.0.13")
+                .HasAnnotation("ProductVersion", "10.0.5")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128);
 
             SqlServerModelBuilderExtensions.UseIdentityColumns(modelBuilder);
@@ -260,8 +260,11 @@ namespace MyFood.Infrastructure.Migrations
 
                     b.Property<string>("Name")
                         .IsRequired()
-                        .HasMaxLength(100)
-                        .HasColumnType("nvarchar(100)");
+                        .HasMaxLength(260)
+                        .HasColumnType("nvarchar(260)");
+
+                    b.Property<int>("Quantity")
+                        .HasColumnType("int");
 
                     b.HasKey("Id");
 
